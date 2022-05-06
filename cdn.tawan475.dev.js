7e9f325f2491b2ebc290ee8d310ff8752588ebd2
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-05-06 10:48:46 +0700

    copy code structure

---
+++ cdn.tawan475.dev.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const fs = require('fs');
+const createError = require('http-errors');
+const express = require('express');
+
+const app = express();
+app.dirname = __dirname;
+require('./libs/middlewares')(app);
+
+let apiRouter = require('./router')(app);
+app.use(apiRouter);
+
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+
+function errorHandler(err, req, res, next) {
+    if (!err) return;
+    res.status(err.status || 500).json({ status: err.status || 500, message: err.message || 'Internal Server Error' });
+};
+app.use(errorHandler);
+
+const options = process.env.NODE_ENV === "production" ? {
+    key: fs.readFileSync('./libs/ssl/private.key.pem'),
+    cert: fs.readFileSync('./libs/ssl/domain.cert.pem'),
+    ca: fs.readFileSync('./libs/ssl/intermediate.cert.pem')
+} : {
+    key: fs.readFileSync('./libs/ssl/localhost/localhost.key'),
+    cert: fs.readFileSync('./libs/ssl/localhost/localhost.crt')
+}
+
+// disabled for http, port = 8003
+// require('http').createServer(app).listen(process.env.PORT, () => {
+//     console.log(`listening at HTTP`)
+// });
+
+require('https').createServer(options, app).listen(process.env.SECURE_PORT, () => {
+    console.log(`listening at HTTPS`)
+});
---
+++ router.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const path = require('path');
+const fs = require('fs');
+
+module.exports = (app) => {
+    const router = Router();
+    return router
+};
