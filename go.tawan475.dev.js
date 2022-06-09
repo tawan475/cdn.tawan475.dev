e23835f01386b841cf671a0e4cd99fa48b168f42
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-06-10 01:30:26 +0700

    update error logging

--- go.tawan475.dev.js
+++ go.tawan475.dev.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const fs = require('fs');
 const createError = require('http-errors');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.dirname = __dirname;
@@ -15,12 +16,24 @@ app.use(function (req, res, next) {
     next(createError(404));
 });
 
+// error handler
+app.use(function (err, req, res, next) {
+    if (!err) return; // no error
+    // render the error page
+	let errorID = uuidv4();
+	let errorRequestLog = `o:${req.get('origin') || "direct"} code:${res.statusCode} ${req.trustedip} ${req.method}:${req.protocol}://${req.get('host')}${req.url}`;
+	let errorMessage = `[${Date.now()}] errorID: ${errorID} | ${errorRequestLog}\r\n${err.stack}`
+	
+	console.log(errorMessage)
+	
+	fs.appendFile(path.join(req.app.dirname, "./error_logs.log"), errorMessage + "\r\n", (err) => {
+		if (err) console.log(err)
+	});
 
-function errorHandler(err, req, res, next) {
-    if (!err) return;
-    res.status(err.status || 500).json({ status: err.status || 500, message: err.message || 'Internal Server Error' });
-};
-app.use(errorHandler);
+	if (!err.status || err.status == 500) err.message = 'Internal Server Error';
+
+    res.status(err.status || 500).json({ status: err.status || 500, errorID: errorID, message: err.message || 'Internal Server Error' });
+});
 
 const options = process.env.NODE_ENV === "production" ? {
     key: fs.readFileSync('/etc/letsencrypt/live/tawan475.dev-0001/privkey.pem'),
